9291fe76afbc6522bece39f216ba7fbd5df4baaa
ngvuthanhnhan <ngvuthanhnhan@gmail.com>
2021-10-20 22:18:40 +0700

    Init project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': ['lexend', ...defaultTheme.fontFamily.sans],
+    },
+    extend: {
+      colors: {
+        'primaryContent': 'rgba(51, 51, 51, 1)',
+      }
+    },
   },
   variants: {
     extend: {},
